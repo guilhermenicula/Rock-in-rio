d7a74d9fa644f2af07f4c272795437db626e9d02
nicula011 <guinicula@gmail.com>
2023-12-12 18:51:45 -0300

    firt commit

---
+++ layout/lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Layout()
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
+
+class Layout extends StatelessWidget {
+  const Layout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Padding(
+          padding: EdgeInsets.all(12.0),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            Icon(Icons.home),
+            Icon(Icons.history),
+            Icon(Icons.account_balance),
+            Icon(Icons.add_a_photo)
+          ])),
+      Stack(alignment: Alignment.center, children: [
+        Container(
+            height: 250,
+            width: double.infinity, // largura total da viewport
+            color: Colors.lightBlue[500]),
+        Container(
+            height: 150,
+            width: 320,
+            color: Colors.indigo.shade800 /* ou: Colors.indigo[800] */
+            ),
+        Image.network(
+            "https://www.unifacef.com.br/wp-content/uploads/2016/02/logo-branco.png")
+      ]),
+      Expanded(
+          child: Container(
+              color: Colors.yellow[200], // Ou: Colors.yellow.shade200
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                  // "Estica" os filhos em toda a largura disponível
+                  // (no caso do Column, no eixo cruzado)
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Exemplo de layout Flutter",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.indigo.shade600,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold)),
+                    const Text("Desenvolvido por Fausto G. Cintra",
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                            fontStyle: FontStyle.italic, fontSize: 16))
+                  ])))
+    ]);
+  }
+}
